aa7b72bdb77feef978bac2a661986080726c4595
kaiwenT <1043595701@qq.com>
2017-10-10 19:00:32 +0800

    将cookie存取，日期转换等函数统一放到tools.js

--- src/main/webapp/js/tools.js
+++ src/main/webapp/js/tools.js
@@ -1,5 +1,15 @@
 /**
- * 存取cookie方法
+ * 工具包
+ */
+//日期对象转换为年月日格式
+function dateFormat(jsondate) {
+	var date = new Date(jsondate.time);
+    return date.getFullYear()+"年"+(1 + date.getMonth())+"月"+date.getDate()+"日";
+    
+}
+
+/**
+ * 存cookie方法
  */
 function setCookie(key, value) {
     var Days = 1; // 此 cookie 将被保存 1 天
@@ -7,7 +17,9 @@ function setCookie(key, value) {
     exp.setTime(exp.getTime() +Days*24*60*60*1000);
     document.cookie = key +"="+ escape (value) + ";expires=" + exp.toGMTString();
 }
-
+/**
+ * 取cookie方法
+ */
 function getCookie(key) {
 //    console.log(document.cookie);
     var arr =document.cookie.match(new RegExp("(^|)"+key+"=([^;]*)(;|$)"));

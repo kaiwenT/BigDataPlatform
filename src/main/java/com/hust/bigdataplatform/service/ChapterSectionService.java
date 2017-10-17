e48e158723a0f0340ed257b7ba2d6bab60fb1a79
qniannian <1141977686@qq.com>
2017-10-17 17:47:32 +0800

    添加章、节的业务层代码

---
+++ src/main/java/com/hust/bigdataplatform/service/ChapterSectionService.java
@@ -0,0 +1,29 @@
+package com.hust.bigdataplatform.service;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Update;
+
+import com.hust.bigdataplatform.model.ChapterSection;
+
+public interface ChapterSectionService {
+	
+	/**
+	 * 增加节
+	 */
+	public int insertSection(ChapterSection chapterSection);
+	
+	/**
+	 * 删除节
+	 */
+	public int deleteSection(String chapterSectionId);
+	/**
+	 * 根据章ID查找节
+	 */
+	public List<ChapterSection> selectByChapterId(String chapterId);
+	/**
+	 * 修改节
+	 */
+	public int Update(ChapterSection chapterSection);
+
+}
---
+++ src/main/java/com/hust/bigdataplatform/service/impl/CourseChapterServiceImpl.java
@@ -0,0 +1,51 @@
+package com.hust.bigdataplatform.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.hust.bigdataplatform.dao.CourseChapterDao;
+import com.hust.bigdataplatform.model.CourseChapter;
+import com.hust.bigdataplatform.service.CourseChapterService;
+@Service 
+public class CourseChapterServiceImpl implements CourseChapterService {
+	@Autowired
+	private CourseChapterDao courseChapterDao;
+
+	/**
+	 * 插入， 返回2提示章的名字为空 
+	 */
+	@Override
+	public int insertChapter(CourseChapter courseChapter) {
+		if (courseChapter.getChapterName()==null) {
+			return 2;
+		}
+		return courseChapterDao.insert(courseChapter);
+	}
+	
+	@Override
+	public int deleteByChapterId(String chapterId, String courseId) {
+		if (chapterId.equals(null) || courseId.equals(null)) {
+			return 0;
+		}
+		return courseChapterDao.deleteById(chapterId, courseId);
+	}
+
+	@Override
+	public List<CourseChapter> selectByCourseID(String CourseId) {
+		if (CourseId.equals(null)) {
+			return null;
+		}
+		return courseChapterDao.selectByCourseId(CourseId);
+	}
+
+	@Override
+	public int update(CourseChapter courseChapter) {
+		if (courseChapter.getChapterName()==null) {
+			return 2;
+		}
+		return courseChapterDao.update(courseChapter);
+	}
+
+}

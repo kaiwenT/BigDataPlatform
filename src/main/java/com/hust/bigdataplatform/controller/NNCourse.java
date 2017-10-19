2e40b5d9c161550346065c86d923d6d1fe19fd1f
qniannian <1141977686@qq.com>
2017-10-19 21:57:27 +0800

    增加章

--- src/main/java/com/hust/bigdataplatform/controller/NNCourse.java
+++ src/main/java/com/hust/bigdataplatform/controller/NNCourse.java
@@ -1,5 +1,9 @@
 package com.hust.bigdataplatform.controller;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.UUID;
+
 import javax.mail.internet.MimeMessage.RecipientType;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,8 +12,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.hust.bigdataplatform.constant.Constant;
 import com.hust.bigdataplatform.model.Course;
 import com.hust.bigdataplatform.model.CourseChapter;
+import com.hust.bigdataplatform.service.CourseChapterService;
 import com.hust.bigdataplatform.service.CourseService;
 import com.hust.bigdataplatform.util.ResultUtil;
 
@@ -19,6 +25,8 @@ public class NNCourse {
 	
 	@Autowired
 	private CourseService courseService;
+	@Autowired
+	private CourseChapterService courseChapterService;
 	
 	@RequestMapping("/SetCourseIntroduce")
 	@ResponseBody
@@ -54,8 +62,23 @@ public class NNCourse {
 			@RequestParam(value="courseName")String courseName)
 	{
 		CourseChapter courseChapter = new CourseChapter();
-		return courseService;
-		
+		String chapterId = UUID.randomUUID().toString(); //章ID
+		courseChapter.setChapterId(chapterId);
+		courseChapter.setChapterName(courseName);
+		courseChapter.setCourseId(courseId);
+		String coursewarePath = Constant.DIRECTORY.COURSEWARE+courseName;
+		courseChapter.setCoursewarePath(coursewarePath);
+		String videoPath = Constant.DIRECTORY.COURSE_VIDEO+courseName;
+		courseChapter.setVideoPath(videoPath);
+		courseChapter.setCreatTime(new Date()); //到此courseChapter封装完毕
+		int status = courseChapterService.insertChapter(courseChapter);
+		if (status==2) {
+			return ResultUtil.errorWithMsg("章的名字不能为空！");
+		}
+		if (status==0) {
+			return ResultUtil.errorWithMsg("添加章失败！");
+		}
+		return ResultUtil.success("成功添加章！");		
 	}
 
 }
